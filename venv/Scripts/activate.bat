3e7ddc416eade46fe4b929596a61070b714a6c3b
Pratham verma <vermapratham623@gmail.com>
2024-10-06 13:43:07 +0530

    auto change

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Digital_Signature_verification_system\venv
+set VIRTUAL_ENV=D:\OLD_Digital_Signature_verification_system\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
